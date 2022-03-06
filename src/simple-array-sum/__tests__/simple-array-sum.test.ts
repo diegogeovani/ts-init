9d8c8b4c9f59d1521e2974ef0a44ad0a6877202c
Diego Santos <diego.santos@olxbr.com>
2022-03-05 23:53:36 -0300

    test: simple array sum module

---
+++ src/simple-array-sum/__tests__/simple-array-sum.test.ts
@@ -0,0 +1,26 @@
+import sum from '../simple-array-sum'
+
+describe('simple-array-sum', () => {
+  it('should return 0 when no argument is passed', () => {
+    expect(sum()).toBe(0)
+  })
+
+  it.each([
+    [[10, 10, 10], 30],
+    [[100, 70, 5], 175],
+    [[0, 8], 8],
+    [[2, 2, 2, 2, 2, 2, 2, 2], 16]
+  ])('should sum numbers', (numbers: number[], expectedSum: number) => {
+    expect(sum(numbers)).toBe(expectedSum)
+  })
+
+  it.each([
+    [[-2, -2], -4],
+    [[-100, -70, -5], -175],
+    [[-1, 0], -1],
+    [[2, -2, 2, 2, 2, -2, 2, 2], 8],
+    [[-2, 4], 2]
+  ])('should sum negative numbers', (numbers: number[], expectedSum: number) => {
+    expect(sum(numbers)).toBe(expectedSum)
+  })
+})
--- src/simple-array-sum/simple-array-sum.js
+++ src/simple-array-sum/simple-array-sum.js
@@ -4,7 +4,9 @@ const args = process.argv.slice(2)
 args[0]
 
 const sum = (numbers) => {
-  return numbers.reduce((previous, current) => parseInt(previous, 10) + parseInt(current, 10), 0)
+  return numbers ? numbers.reduce((previous, current) => parseInt(previous, 10) + parseInt(current, 10), 0) : 0
 }
+
+export default sum
 
 console.log(sum(args))
